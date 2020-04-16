2540a109f91fd42252198850a12958dc392f8e6a
pkowalecki <kowalecki13pl@o2.pl>
2020-04-16 19:21:48 +0200

    Commit

--- src/main/java/pl/kowalecki/soaplab1/WebClientConfig.java
+++ src/main/java/pl/kowalecki/soaplab1/WebClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+
 @Configuration
 public class WebClientConfig {
 
